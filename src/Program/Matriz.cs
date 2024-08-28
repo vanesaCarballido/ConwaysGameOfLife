c07521973627bf25213522310ca2ba12e1f2b1d5
isaparedes <isabela.paredes@correo.ucu.edu.uy>
2024-08-28 10:20:02 -0300

    agrego matriz

---
+++ src/Program/Matriz.cs
@@ -0,0 +1,20 @@
+using System.IO;
+namespace Ucu.Poo.GameOfLife;
+
+public class Matriz
+{
+    string url = "ruta del archivo";
+    string content = File.ReadAllText(url);
+    string[] contentLines = content.Split('\n');
+    bool[,] board = new bool[contentLines.Length, contentLines[0].Length];
+        for (int  y=0; y<contentLines.Length;y++)
+    {
+        for (int x=0; x<contentLines[y].Length; x++)
+        {
+            if(contentLines[y][x]=='1')
+            {
+                board[x,y]=true;
+            }
+        }
+    }
+}
